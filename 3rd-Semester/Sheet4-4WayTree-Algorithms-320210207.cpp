01ce031f02faec885d67ee843eded1b2d37e2cb5
37743 <yousef.ibrahim.gomaa@gmail.com>
2022-11-17 21:11:23 +0200

    Updated 4WayTree

--- 3rd-Semester/Sheet4-4WayTree-Algorithms-320210207.cpp
+++ 3rd-Semester/Sheet4-4WayTree-Algorithms-320210207.cpp
@@ -1,306 +1,141 @@
-
-#include<iostream>
+// Yousef Ibrahim Gomaa Mahmoud - 320210207 - Section 10
 #include<vector>
-#include<algorithm>
 #include<queue>
-#include<math.h>
+#include<iostream>
+#include<algorithm>
 
 using namespace std;
 
-struct mnode{
-    vector<int> key;
-    vector<mnode*> next;
+// Implementation of 4-Way Tree in C++
 
-    mnode(int m,int key){
-        for(int i = 0;i<m;i++){
+struct Node{
+    int children;
+    vector<int> value;
+    vector<Node*> next;
+
+    Node(int value){ // Creates a tree node of size 4 indices.
+        for(int i = 0;i<4;i++){
             this->next.push_back(NULL);
         }
-        this->key.push_back(key);
+        this->value.push_back(value);
+        this->children = 0;
     }
 };
 
 
-class mway{
-    int m,h;
-    mnode* root;
-public:
-    mway(int m){
+class FWTree{
+    int h;
+    Node* root;
+    public:
+
+    FWTree(int count){
         root = NULL;
-        this->m = m;
     }
 
-    mnode* insertion(int item,mnode* ptr){
+    // Print In-order Traversal
+    void TreePrint(){
         if(root == NULL){
-            root = new mnode(m,item);
-            return root;
-        }
-        if(ptr == NULL){
-            ptr = new mnode(m,item);
-            return ptr;
-        }
-        mnode* newmnode = new mnode(m,item);
-        if(ptr->key.size() == m-1){
-            for(int i =0 ;i< m-1;i++){
-                if(item < ptr->key[i]){
-                    ptr->next[i] = insertion(item,ptr->next[i]);
-                    break;
-                }   
-                else if(item > ptr->key[m-2]){
-                    ptr->next[m-1] = insertion(item,ptr->next[m-1]);
-                    break;
-                }
-            }
-        }
-        else{        
-            ptr->key.push_back(item);
-            sort(ptr->key.begin(),ptr->key.end());
-        }
-        return ptr;
-    }
-
-    int findsuc(mnode* ptr){
-        if(ptr->next[0]==NULL){
-            return ptr->key[0];
-        }
-        else{
-            return findsuc(ptr->next[0]);
-        }
-    }
-    mnode* deletion(int item,mnode*ptr){
-        if(root == NULL){
-            cout<<"\nempty tree.";
-            return root;
-        }
-        if(ptr == NULL){
-            return ptr;
-        }
-    
-        for(int i = 0 ; i < ptr->key.size() ; i++){
-            if(ptr->key[i] == item){
-                if(ptr->next[i+1] != NULL){
-                    int suc = findsuc(ptr->next[i+1]);
-                    ptr->key[i] = suc;
-                    ptr->next[i+1] = deletion(suc,ptr->next[i+1]);
-                    return ptr;
-                }
-                else{
-                    if(i < ptr->key.size()-1){
-                        for(int j = i;j < ptr->key.size()-1;j++){
-                            ptr->key[j] = ptr->key[j+1];
-                            ptr->next[j+1] = ptr->next[j+2];
-                        }
-                    }
-                    ptr->key.pop_back();
-                    ptr->next[ptr->key.size()] = NULL;
-                    return ptr;
-                }
-            }    
-        }
-        if(item < ptr->key[0]){
-            ptr->next[0] = deletion(item,ptr->next[0]);
-            return ptr;
-        }
-        for(int i =0;i<ptr->next.size();i++){
-            if(i != ptr->key.size()-1){
-                if(item > ptr->key[i] && item < ptr->key[i+1]){
-                    ptr->next[i+1] = deletion(item,ptr->next[i+1]);
-                    return ptr;
-                }
-            }
-            else{
-                if(item > ptr->key[i]){
-                    ptr->next[i+1] = deletion(item,ptr->next[i+1]);
-                    return ptr;
-                }
-            }
-        }
-    }
-
-    void caldeletion(int item){
-        mnode * ptr = deletion(item,root);
-        if(ptr == NULL){
-            cout<<"\nitem not found";
+            cout<<"\nThere are no nodes in the tree.";
             return;
         }
-        else{
-            cout<<"\nitem found and deleted.";
-        }
-        return;
-    }
-
-    void traversal(){
-        queue<mnode*> q1;
-        if(root == NULL){
-            cout<<"\ntree is empty.";
-            return;
-        }
+        queue<Node*> q1; // Using queue library.
         q1.push(root);
         q1.push(NULL);
         while(!q1.empty()){
-            mnode * ptr = q1.front();
+            Node * ptr = q1.front();
             q1.pop();
             if(ptr == NULL){
                 cout<<"\n";
                 continue;
             }
-            for(int i =0;i<ptr->key.size();i++){
-                cout<<ptr->key[i]<<"  ";
-            }
-            for(int i =0;i<ptr->key.size()+1;i++){
-                q1.push(ptr->next[i]);
-            }
+            for(int i =0;i<ptr->value.size();i++) cout<<ptr->value[i]<<" ";
+            for(int i =0;i<ptr->value.size()+1;i++) q1.push(ptr->next[i]);
         }
         return;
     }
 
-    int height(){
-        h = 0;
-        queue<mnode*> q;
-        mnode * ptr = root;
-        q.push(ptr);
-        if(root == NULL)
-            return 0;
-        int nodecount;
-    
-        while(!q.empty()){
-            nodecount=q.size();
-            for(int i =0;i<nodecount;i++){
-                ptr = q.front();
-                q.pop();
-                for( int j = 0;j < ptr->next.size();j++){
-                    if(ptr->next[j] != NULL)
-                        q.push(ptr->next[j]);
-                }
-            }
-            h++;
+    Node* Searchnode(int val, Node* node, int* pos)
+    {
+        if (node == NULL) return NULL;
+        else {
+            if (Foundnode(val, node, pos)) return root;
+            else return Searchnode(val, node->next[*pos], pos);
         }
-        cout<<"\nheight is:"<<h;
-        return h;    
     }
     
-    vector<mnode*> a;
-    void getlevel(mnode * ptr,int level){
+    bool Foundnode(int val, Node* node, int* pos)
+    {
+        if (val < node->value[1]) {
+            *pos = 0;
+            return 0;
+        }
+        else {
+            *pos = node->children;
+            while ((val < node->value[*pos]) && *pos > 1) (*pos)--;
+            if (val == node->value[*pos])
+                return true;
+            else
+                return false;
+        }
+    }
+    vector<Node*> nodeVec;
+    void getlevel(Node * ptr,int level){
         if (level == 0) {
-			a.push_back(ptr);
+			nodeVec.push_back(ptr);
 		}
 		else {
-			if (ptr != NULL) {
-                for(int i =0;i<m;i++){
-				    getlevel(ptr->next[i], level - 1);
-                }
-			}
-			else {
-                for(int i =0;i<m;i++){
-				    getlevel(NULL, level - 1);
-                }
-			}
+			if (ptr != NULL) for(int i =0;i<4;i++) getlevel(ptr->next[i], level - 1);
+			else for(int i =0;i<4;i++) getlevel(NULL, level - 1);
 		}
     }
-
-    void printarr() {
-		int k = 0, gl, ls, ms, t = 0;
-		int n = h, j;
-		cout << "in print tree\n";
-
-		for (int i = 0; i < h; i++) {
-
-			ls = pow(2, n - i) - 1;
-			ms = pow(2, n - i + 1) - 1;
-			gl = pow(2, max(0, n - i - 1));
-
-			for (j = 0; j < ls; j++)
-				cout << " ";
-
-			getlevel(root, i);
-
-			for (int y = t; y < a.size(); y++) {
-                if (a[y] == NULL)
-                    for( int b =0;b<m-1;b++)    
-                        cout << " ";
-                else
-                    {
-                        for( int b =0;b<a[y]->key.size();b++){
-                            cout << a[y]->key[b];
-                        }
-                        if(a[y]->key.size() < m-1){
-                            int si = a[y]->key.size()-1;
-                            while(si <m-1){
-                                cout<<" ";
-                                si++;
-                            }
-                        }
-                    }
-
-				//for (j = 0; j < ms ; j++)
-					cout << " ";
-			}
-
-            for (j = 0; j<gl-1; j++) {
-				cout << "\n";
-
-				for (int p = 0; p < ls - j; p++)
-					cout << " ";
-				for (int l = t; l < a.size(); l++) {
-					if (a[l] != NULL) {
-                        for(int b =0;b < m-1;b++){
-                            if (a[l]->next[b] != NULL)
-							    cout << "/";
-						    else
-							    cout << " ";
-						    for (int p = 0; p < (2 * j + 1); p++)
-							    cout << " ";
-						    if (a[l]->next[b+1] != NULL)
-							    cout << "\\";
-						    else
-                                cout << " ";
-                            for (int mss = 0; mss < (ms - 2 * j - 2); mss++) {
-                                cout << " ";
-                            }
-                        }
-						
-                    }
-                    else {
-                        for(int b =0;b<m-1;b++){
-                            cout << " ";
-	    					for (int p = 0; p < (2 * j + 1); p++)
-		    					cout << " ";
-			    			cout << " ";
-                        }
-                        for (int mss = 0; mss < (ms - 2 * j - 2); mss++) {
-					    	cout << " ";
-                        }
-                    }
+    
+    Node* TreeInsert(int item,Node* ptr){
+        if(root == NULL){ // Case of empty tree, creates the first node.
+            root = new Node(item);
+            return root;
+        }
+        if(ptr == NULL){
+            ptr = new Node(item);
+            return ptr;
+        }
+        Node* newNode = new Node(item);
+        if(ptr->value.size() == 3) // Inserts in node's children. (If full)
+            for(int i =0 ;i< 3;i++){
+                if(item < ptr->value[i]){
+                    ptr->next[i] = TreeInsert(item,ptr->next[i]);
+                    ptr->children += 1;
+                    break;
+                }   
+                else if(item > ptr->value[2]){
+                    ptr->next[3] = TreeInsert(item,ptr->next[3]);
+                    ptr->children += 1;
+                    break;
                 }
             }
-            cout<<"\n";
-            t = a.size();
+        else{        
+            ptr->value.push_back(item);
+            sort(ptr->value.begin(),ptr->value.end()); // Re-arrange vector
         }
-        a.erase(a.begin(),a.end());
+        return ptr;
     }
-
-    void calins(int item){
-        insertion(item,root);
-    }
-
-    void caltra(){
-        traversal();
-    }
-    
+    // Insert Method
+    void Insert(int item){TreeInsert(item,root);}
+    // Search Method
+    void Print(){TreePrint();}
+    void Search(int val, int* pos){Searchnode(val,root,pos);}
 };
 
 int main(){
-    int i ;
-    mway t1(4);
-    int arr[] = {60, 30, 90, 10, 50, 80, 100, 20, 40, 70, 39, 38, 37, 36, 35, 34, 33, 32};
-    for(int l =0; l < 18; l++)
-    {
-      t1.calins(arr[l]);
-    }
-
-    t1.caltra();
+    FWTree testTree(0); // Initialize tree
+    // Given array:
+    int arr1[] = {60, 30, 90, 10, 50, 80, 100, 20, 40, 70, 39, 38, 37, 36, 35, 34, 33, 32}; 
+    int arrSize = sizeof(arr1)/sizeof(arr1[0]);
+    for(int i =0; i < arrSize; i++) {testTree.Insert(arr1[i]); cout<<"Inserted node: "<<arr1[i]<<"\n";}
+    cout<<"\n-----------------\n";
+    int* position=0;
+    cout<<"\nPrinting the tree (I-O T):\n\n";
+    testTree.Print();
+    // testTree.Search(30,position);
+    // cout<<"Position of found element is: "<<position;
     cout<<"\n";
-    t1.height();
-    cout<<"\n";
-    
-    return 0;
 }
